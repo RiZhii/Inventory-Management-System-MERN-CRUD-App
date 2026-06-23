bf6ccc00916c0145e62f76b2cde39c0b4dacdb69
RiZhii <dhuruvarishi7@gmail.com>
2026-06-23 21:24:06 +0530

    changes

--- Frontend/inventory_management_system/src/components/Home.js
+++ Frontend/inventory_management_system/src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Home() {
   return (
     <div className='container-fluid p-5'>
-        <h2>Go to Products page and manage inventory.</h2>
+        <h2>Go to Products .</h2>
     </div>
   )
 }
