6ea43d7f3f00648d4a988dd7bdd915f89b224b5a
RiZhii <dhuruvarishi7@gmail.com>
2026-06-24 19:37:44 +0530

    small

--- Frontend/inventory_management_system/src/components/Home.js
+++ Frontend/inventory_management_system/src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Home() {
   return (
     <div className='container-fluid p-5'>
-        <h2>Go to Products .</h2>
+        <h2>Go to Products sect .</h2>
     </div>
   )
 }
